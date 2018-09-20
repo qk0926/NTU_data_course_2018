018ba0ab9fbc78f2fc74fc549356b4c282231a3d
qk0926 <qk0926@gmail.com>
2018-09-20 11:54:49 +0800

    week2

--- week_2/hw_2/b05401102 crawler.R
+++ week_2/hw_2/b05401102 crawler.R
@@ -1,6 +1,6 @@
 library(rvest)
 i<-1
-for(i in 1:10){
+for(i in 1:9){
   page<-paste("https://www.amazon.com/s/ref=sr_pg_2?fst=as%3Aon&rh=n%3A230659011%2Cn%3A172282%2Cn%3A%21493964%2Cn%3A541966%2Cn%3A13896617011%2Ck%3Aacer&page=",i,"&bbn=230659011&keywords=acer&ie=UTF8&qid=1537412681",sep = "")
   res<-read_html(page)
   raw<-res %>% html_nodes("h2") %>% html_attr('data-attribute')
